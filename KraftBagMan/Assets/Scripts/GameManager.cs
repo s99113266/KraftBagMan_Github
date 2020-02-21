c0b4c4cb379d34ed2e3e8748a914c4d489f2f456
s99113266 <41054903+s99113266@users.noreply.github.com>
2020-02-21 21:36:48 +0800

    資料中心

---
+++ KraftBagMan/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public PlayData _PlayData;
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ KraftBagMan/Assets/Scripts/PlayManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "玩家資料", menuName = "CHERRY/PLAY 素質設定")]
+public class PlayData : ScriptableObject
+{
+    [Header("受攻擊次數"), Range(3, 15)]
+    public int Hp;
+    public int MaxHp;
+
+    [Header("移動速度"), Range(10, 5000)]
+    public float Speed;
+
+    [Header("跳躍高度"), Range(10, 5000)]
+    public float JumpHeight;
+}
